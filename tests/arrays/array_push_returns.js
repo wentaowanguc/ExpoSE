fd5d5145d12de70105838ea90c9dd7ad95f44af5
Arran France <arranf@gmail.com>
2018-03-19 19:32:39 +0000

    Add push() returns test
    TODO pop() returns test

---
+++ tests/arrays/array_push_returns.js
@@ -0,0 +1,13 @@
+/* Copyright (c) Royal Holloway, University of London | Contact Blake Loring (blake@parsed.uk), Duncan Mitchell (Duncan.Mitchell.2015@rhul.ac.uk), or Johannes Kinder (johannes.kinder@rhul.ac.uk) for details or support | LICENSE.md for license details */
+
+"use strict";
+
+var q = symbolic UnderTest initial [0, 1];
+var p = symbolic Value initial 5;
+
+var v = q.push(p);
+if (v === p) {
+  console.log('Reached');
+} else {
+  throw 'array_push_returns: Unreachable - pushed value not equal to return of push';
+}
