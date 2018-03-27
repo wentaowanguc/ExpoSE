c71b5cd6fbbd6c0d6afcabdaae0d209febc2347b
Arran France <arranf@gmail.com>
2018-03-27 14:57:54 +0100

    polyfill harness

---
+++ Report/polyfill_harness.js
@@ -0,0 +1,30 @@
+"use strict";
+
+function testPolyfill(testFunction, prototypeFunction, base, args) {
+    console.log('Base ' + base + ', args ' + args);
+  
+    var result1;
+    var result2;
+  
+    try {
+      result1 = testFunction.apply(base, args);
+    } catch(e) {
+      // Empty catch
+    }
+  
+    try {
+      result2 = prototypeFunction.apply(base, args);
+    } catch(e) {
+      // Empty catch
+    }
+  
+    result1 = JSON.stringify(result1);
+    result2 = JSON.stringify(result2);
+
+    if (result1 !== result2) {
+      throw 'Alert -> Error: polyfill: \'' + result1 + '\' prototype: \'' +  result2 + '\'';
+    } 
+  };
+
+var from = require('array-from');
+testPolyfill(from, Array.from, symbolic thisArg, [symbolic Needle]);
